84b3979887f0856002c04209a1b10a2bcf572938
Bertrand Kerautret <bertrand.kerautret@univ-lyon2.fr>
2023-11-16 00:58:57 +0100

    base read texture

--- src/DGtal/io/readers/MeshReader.h
+++ src/DGtal/io/readers/MeshReader.h
@@ -160,8 +160,18 @@ public:
   /// @return 'true' if both reading the input stream was ok and the
   /// created mesh is ok.
   static
-  std::map<std::string, DGtal::Color> readMaterial( std::istream & input);
-    
+  std::map<std::string, DGtal::Color > readMaterial( std::istream & input);
+
+   /// Reads an input material file associated ot an OBJ file format and outputs the
+  /// corresponding materials as a map assiating name and diffuse color.
+  ///
+  /// @param[in,out] input the input stream where the OBJ file is read.
+  ///
+  /// @return 'true' if both reading the input stream was ok and the
+  /// created mesh is ok.
+  static
+  std::map<std::string, std::string > readTextureMaterial( std::istream & input);
+
 
 
 }; // end of class MeshReader
